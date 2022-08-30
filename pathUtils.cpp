6a46c0615436539d84c1f0736cc0059055de306f
Reupen Shah <reupen@users.noreply.github.com>
2022-08-30 19:48:54 +0100

    Merge changes from SDK 2022-08-29

--- pathUtils.cpp
+++ pathUtils.cpp
@@ -274,9 +274,7 @@ string validateFileName(string name, bool allowWC, bool preserveExt, charReplace
 	// replaceIllegalNameChars may remove chars exposing illegal prefix/suffix chars
 	name = replaceIllegalNameChars(name, allowWC, replaceIllegalChar);
 	if (name.length() > 0 && !allowWC) {
-		pfc::string8 lstIllegal = " ";
-		if (!preserveExt) lstIllegal += ".";
-
+		const char* lstIllegal = preserveExt ? "" : " .";
 		name = trailingSanity(name, preserveExt, lstIllegal);
 	}
 
